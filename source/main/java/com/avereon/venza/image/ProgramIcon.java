2d894266a0d287d149b9d4552f630243df099b42
Mark Soderquist <mark@soderquist.net>
2019-12-14 20:31:06 -0700

    Initial revision

---
+++ source/main/java/com/avereon/venza/image/ProgramIcon.java
@@ -0,0 +1,9 @@
+package com.avereon.venza.image;
+
+public abstract class ProgramIcon extends ProgramImage {
+
+	public double getSize() {
+		return getWidth();
+	}
+
+}
---
+++ source/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.avereon.venza {
+	requires com.avereon.zevra;
+	requires java.logging;
+	requires javafx.controls;
+	requires javafx.swing;
+	requires org.slf4j;
+
+	exports com.avereon.venza.color;
+	exports com.avereon.venza.font;
+	exports com.avereon.venza.image;
+	exports com.avereon.venza.javafx;
+}
