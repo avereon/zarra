d62d9f45201b357e3dc36e429bb1f37350f0e0ae
Mark Soderquist <mark@soderquist.net>
2020-02-24 11:52:52 -0700

    Add an event watcher

---
+++ source/main/java/com/avereon/venza/javafx/FxEventWatcher.java
@@ -0,0 +1,78 @@
+package com.avereon.venza.javafx;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.TimeoutException;
+
+public class FxEventWatcher<T extends Event> implements EventHandler<T> {
+
+	public static final long DEFAULT_WAIT_TIMEOUT = 10000;
+
+	private Queue<T> events = new ConcurrentLinkedQueue<>();
+
+	@Override
+	public synchronized void handle( T event ) {
+		events.offer( event );
+		notifyAll();
+	}
+
+	public void waitForEvent( EventType<? extends T> type ) throws InterruptedException, TimeoutException {
+		waitForEvent( type, DEFAULT_WAIT_TIMEOUT );
+	}
+
+	public void waitForNextEvent( EventType<? extends T> type ) throws InterruptedException, TimeoutException {
+		waitForNextEvent( type, DEFAULT_WAIT_TIMEOUT );
+	}
+
+	/**
+	 * Wait for an event of a specific class to occur. If the event has already
+	 * occurred this method will return immediately. If the event has not
+	 * already occurred then this method waits until the next event occurs, or
+	 * the specified timeout, whichever comes first.
+	 *
+	 * @param type The event type to wait for
+	 * @param timeout How long, in milliseconds, to wait for the event
+	 * @throws InterruptedException If the timeout is exceeded
+	 */
+	public synchronized void waitForEvent( EventType<? extends T> type, long timeout ) throws InterruptedException, TimeoutException {
+		boolean shouldWait = timeout > 0;
+		long start = System.currentTimeMillis();
+		long duration = 0;
+
+		while( shouldWait && findNext( type ) == null ) {
+			wait( timeout - duration );
+			duration = System.currentTimeMillis() - start;
+			shouldWait = duration < timeout;
+		}
+		duration = System.currentTimeMillis() - start;
+
+		if( duration >= timeout ) throw new TimeoutException( "Timeout waiting for event " + type );
+	}
+
+	/**
+	 * Wait for the next event of a specific class to occur. This method always
+	 * waits until the next event occurs, or the specified timeout, whichever
+	 * comes first.
+	 *
+	 * @param type The event class to wait for
+	 * @param timeout How long, in milliseconds, to wait for the event
+	 * @throws InterruptedException If the timeout is exceeded
+	 */
+	public synchronized void waitForNextEvent( EventType<? extends T> type, long timeout ) throws InterruptedException, TimeoutException {
+		findNext( type );
+		waitForEvent( type, timeout );
+	}
+
+	private Event findNext( EventType<? extends Event> type ) {
+		Event event;
+		while( (event = events.poll()) != null ) {
+			if( event.getEventType() ==  type ) return event;
+		}
+		return null;
+	}
+
+}
