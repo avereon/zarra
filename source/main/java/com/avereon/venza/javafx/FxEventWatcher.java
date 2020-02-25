2387b6977ec6200db575a24e59fbc0bdfa676d14
Mark Soderquist <mark@soderquist.net>
2020-02-24 18:45:31 -0700

    Shorten event watcher default timeout

--- source/main/java/com/avereon/venza/javafx/FxEventWatcher.java
+++ source/main/java/com/avereon/venza/javafx/FxEventWatcher.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeoutException;
 
 public class FxEventWatcher implements EventHandler<Event> {
 
-	public static final long DEFAULT_WAIT_TIMEOUT = 10000;
+	public static final long DEFAULT_WAIT_TIMEOUT = 2000;
 
 	private Queue<Event> events = new ConcurrentLinkedQueue<>();
 
