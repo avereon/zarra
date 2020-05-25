2b6705310165182e1370bc3ff72bdf89a37fdb40
Mark Soderquist <mark@soderquist.net>
2020-05-24 19:07:54 -0600

    Continue work on flat icons

---
+++ source/test/java/com/avereon/venza/image/RenderedImageWriterTest.java
@@ -0,0 +1,53 @@
+package com.avereon.venza.image;
+
+import com.avereon.venza.icon.BrokenIcon;
+import com.avereon.venza.icon.RenderedIcon;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RenderedImageWriterTest {
+
+	private Path targetFolder = Paths.get( "target", "images" );
+
+	@Test
+	void testSavePng() throws Exception {
+		runTest( "testicon.png", ( path ) -> new RenderedImageWriter().save( new BrokenIcon(), path ) );
+	}
+
+	@Test
+	void testSaveIco() throws Exception {
+		runTest( "testicon.ico", ( path ) -> new RenderedImageWriter().save( new BrokenIcon(), path ) );
+	}
+
+	@Test
+	void testSavePngWithOffset() throws Exception {
+		runTest( "testiconoffset.png", ( path ) -> {
+			RenderedIcon icon = new BrokenIcon();
+			double size = icon.getSize();
+			double pad = 0.2 * size;
+			icon.relocate( pad, pad );
+			new RenderedImageWriter().save( icon, path, size + 2 * pad, size + 2 * pad );
+		} );
+	}
+
+	private void runTest( String name, Generator generator ) throws Exception {
+		Path path = targetFolder.resolve( name );
+		Files.deleteIfExists( path );
+		assertFalse( Files.exists( path ) );
+		generator.run( path );
+		assertTrue( Files.exists( path ) );
+	}
+
+	private interface Generator {
+
+		void run( Path path ) throws Exception;
+
+	}
+
+}
