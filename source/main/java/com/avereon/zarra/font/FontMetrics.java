dabb09afb959ed638d2406d9e9c20ba9e5eb255d
msoderquist <msoderquist@mastercontrol.com>
2024-04-18 15:06:50 -0600

    Add FontMetrics.computeStringBounds()

--- source/main/java/com/avereon/zarra/font/FontMetrics.java
+++ source/main/java/com/avereon/zarra/font/FontMetrics.java
@@ -3,6 +3,7 @@ package com.avereon.zarra.font;
 import javafx.geometry.Bounds;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextBoundsType;
 import lombok.Getter;
 
 public class FontMetrics {
@@ -23,10 +24,18 @@ public class FontMetrics {
 		text.setFont( font );
 
 		// Ascent and decent are inverted
+		text.setBoundsType( TextBoundsType.LOGICAL );
 		Bounds bounds = text.getLayoutBounds();
 		ascent = -bounds.getMinY();
 		descent = -bounds.getMaxY();
 		spacing = bounds.getHeight();
+
+		text.setBoundsType( TextBoundsType.VISUAL );
+	}
+
+	public Bounds computeStringBounds( String text ) {
+		this.text.setText( text );
+		return this.text.getLayoutBounds();
 	}
 
 	public double computeStringWidth( String text ) {
--- source/test/java/com/avereon/zarra/font/FontMetricsTest.java
+++ source/test/java/com/avereon/zarra/font/FontMetricsTest.java
@@ -1,5 +1,6 @@
 package com.avereon.zarra.font;
 
+import javafx.geometry.Bounds;
 import javafx.scene.text.Font;
 import org.assertj.core.data.Percentage;
 import org.junit.jupiter.api.Test;
@@ -8,14 +9,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class FontMetricsTest {
 
+	private FontMetrics metrics = new FontMetrics( Font.getDefault() );
+
+	@Test
+	void testFontMetrics() {
+		assertThat( metrics ).isNotNull();
+		assertThat( metrics.getAscent() ).isCloseTo( 12.06689453125, Percentage.withPercentage( 1 ) );
+		assertThat( metrics.getDescent() ).isCloseTo( -3.06591796875, Percentage.withPercentage( 1 ) );
+		assertThat( metrics.getSpacing() ).isCloseTo( 15.1328125, Percentage.withPercentage( 1 ) );
+	}
+
+	@Test
+	void testComputeStringBounds() {
+		Bounds bounds = metrics.computeStringBounds( "Hello World" );
+		assertThat( bounds.getMinX() ).isCloseTo( 1.27587890625, Percentage.withPercentage( 1 ) );
+		assertThat( bounds.getMinY() ).isCloseTo( -9.876953125, Percentage.withPercentage( 1 ) );
+		assertThat( bounds.getWidth() ).isCloseTo( 72.642578125, Percentage.withPercentage( 1 ) );
+		assertThat( bounds.getHeight() ).isCloseTo( 10.06103515625, Percentage.withPercentage( 1 ) );
+	}
+
 	@Test
 	void testComputeStringWidth() {
-		assertThat( new FontMetrics( Font.getDefault() ).computeStringWidth( "Hello World" ) ).isCloseTo( 75, Percentage.withPercentage( 1 ) );
+		assertThat( metrics.computeStringWidth( "Hello World" ) ).isCloseTo( 72.642578125, Percentage.withPercentage( 1 ) );
 	}
 
 	@Test
 	void testComputeStringHeight() {
-		assertThat( new FontMetrics( Font.getDefault() ).computeStringHeight( "Hello World" ) ).isCloseTo( 15, Percentage.withPercentage( 1 ) );
+		assertThat( metrics.computeStringHeight( "Hello World" ) ).isCloseTo( 10.06103515625, Percentage.withPercentage( 1 ) );
 	}
 
 }
