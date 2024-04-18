2901c01e37c83613d0d05a256e8fbb7b72f185d5
msoderquist <msoderquist@mastercontrol.com>
2024-04-18 11:45:40 -0600

    Add FontMetrics.computeStringHeight()

--- source/test/java/com/avereon/zarra/font/FontMetricsTest.java
+++ source/test/java/com/avereon/zarra/font/FontMetricsTest.java
@@ -10,12 +10,12 @@ public class FontMetricsTest {
 
 	@Test
 	void testComputeStringWidth() {
-		assertThat( new FontMetrics( Font.getDefault() ).computeStringWidth( "Hello World" ) ).isCloseTo( 75, Percentage.withPercentage( 5 ) );
+		assertThat( new FontMetrics( Font.getDefault() ).computeStringWidth( "Hello World" ) ).isCloseTo( 75, Percentage.withPercentage( 1 ) );
 	}
 
 	@Test
 	void testComputeStringHeight() {
-		assertThat( new FontMetrics( Font.getDefault() ).computeStringHeight( "Hello World" ) ).isCloseTo( 15, Percentage.withPercentage( 5 ) );
+		assertThat( new FontMetrics( Font.getDefault() ).computeStringHeight( "Hello World" ) ).isCloseTo( 15, Percentage.withPercentage( 1 ) );
 	}
 
 }
